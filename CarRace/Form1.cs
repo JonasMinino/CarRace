cefc6be84d8d4b0f8ad1dbc2df6e0863a0350075
jonasminino <jonasminino@gmail.com>
2021-01-11 12:47:13 -0500

    Added movement to the enemies.

--- CarRace/Form1.cs
+++ CarRace/Form1.cs
@@ -20,6 +20,7 @@ namespace CarRace
         private void timer1_Tick(object sender, EventArgs e)
         {
             moveLine(2);
+            moveEnemy(2);
         }
         /// <summary>
         /// Adds movement to the center lanes to a specific speed. 
@@ -37,6 +38,19 @@ namespace CarRace
             else pictureBox4.Top += speed;
             if (pictureBox5.Top >= 400) pictureBox5.Top = 0;
             else pictureBox5.Top += speed;            
+        }
+        /// <summary>
+        /// Adds movement to the enemy objects.
+        /// </summary>
+        /// <param name="speed"></param>
+        public void moveEnemy(int speed)
+        {
+            if (enemy1.Top >= 400) enemy1.Top = 0;
+            else enemy1.Top += speed;
+            if (enemy2.Top >= 400) enemy2.Top = 0;
+            else enemy2.Top += speed;
+            if (enemy3.Top >= 400) enemy3.Top = 0;
+            else enemy3.Top += speed;
         }
         /// <summary>
         /// Adds movement to the main car object at a specific speed;
